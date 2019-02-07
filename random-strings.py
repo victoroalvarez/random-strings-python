50511122d6a5bbc51e347ad268fc268994c51e1b
Víctor O. Álvarez <voalvarez23@gmail.com>
2019-02-07 14:31:49 -0400

    Update random-strings.py

--- random-strings.py
+++ random-strings.py
@@ -1,5 +1,12 @@
+# libraries import
 import string
 import random
+
+# String generator function, takes an integer argument which determines string length
+# and another with all alpha-numeric characters
 def rs_generator(string_size=12, alphanumeric_characters=string.ascii_lowercase + string.digits):
+	# returns alpha numeric string of particular string length determined by the string_size parameter
 	return ''.join(random.SystemRandom().choice(alphanumeric_characters) for _ in range(string_size))
+
+# Calls rs_generator function with 6 as a integer argument for setting string length
 print(rs_generator(6))
